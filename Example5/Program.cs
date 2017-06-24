ddf49b751597050ebf2d7267f4e028ca867a8268
Kerry Jiang <kerry-jiang@hotmail.com>
2017-06-24 10:18:24 -0700

    added more examples

---
+++ Example5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Example5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var scoreSheet = new List<KeyValuePair<string, int>>();
+
+            foreach (var item in args)
+            {
+                var arr = item.Split(':');
+                
+                var record = new KeyValuePair<string, int>(arr[0], int.Parse(arr[1]));
+
+                scoreSheet.Add(record);
+            }
+
+            Console.WriteLine("Please input a student name to search score:");
+
+            var name = Console.ReadLine();
+
+            foreach (var record in scoreSheet)
+            {
+                if(record.Key == name)
+                {
+                    Console.WriteLine("The score of " + name + " is " + record.Value);
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Example6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Example6
+{
+    // Fibonacci Numbers: F(n) = F(n-1) + F(n-2)
+    class Program
+    {
+        public static int CalculateFibonacci(int n)  
+        {  
+                if (n == 0)
+                    return 0; //To return the first Fibonacci number
+
+                if (n == 1)
+                    return 1; //To return the second Fibonacci number
+
+                return CalculateFibonacci(n - 1) + CalculateFibonacci(n - 2);  
+        }  
+
+        static void Main(string[] args)
+        {
+            // CalculateFibonacci base on the first parameter
+            var num = int.Parse(args[0]);
+            
+            Console.WriteLine(CalculateFibonacci(num));
+        }
+    }
+}
