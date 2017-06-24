0d8915ba65a578a66363e3b5a9d2fda82f8b0064
Kerry Jiang <kerry-jiang@hotmail.com>
2017-06-24 09:36:53 -0700

    added example 4

---
+++ Example4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Example4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new List<string>();
+
+            while (true)
+            {
+                Console.WriteLine("Please input a student name:");
+
+                var name = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(name))
+                    break;
+
+                names.Add(name);
+            }
+
+            foreach (var name in names)
+            {
+                Console.WriteLine(name + ":" + name.Length);
+            }
+        }
+    }
+}
