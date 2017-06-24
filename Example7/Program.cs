92f371625509ad3e7943c01f736bbe01c375bf3b
Kerry Jiang <kerry-jiang@hotmail.com>
2017-06-24 11:55:51 -0700

    more exmaples

---
+++ Example7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Example7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var fileStream = new FileStream("score.csv", FileMode.Open, FileAccess.Read))
+            {
+                using (var reader = new StreamReader(fileStream, Encoding.UTF8, true))
+                {
+                    while (reader.Peek() >= 0)
+                    {
+                        Console.WriteLine(reader.ReadLine());
+                    }
+                }
+            }            
+        }
+    }
+}
