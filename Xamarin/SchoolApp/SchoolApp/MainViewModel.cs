67726bd5ea56e3ccc0e419f823262de51a3ab2fd
Kerry Jiang <kerry-jiang@hotmail.com>
2017-07-30 11:39:34 -0700

    updated UI

--- Xamarin/SchoolApp/SchoolApp/MainViewModel.cs
+++ Xamarin/SchoolApp/SchoolApp/MainViewModel.cs
@@ -38,6 +38,8 @@ namespace SchoolApp
                     {
                         Students.Add(s);
                     }
+
+                    StudentCount = students.Count;
                 }
             }
         }
